80cfa1bcb2b800729ee207270bd9e384f756fc23
Adrian Ganzo <adrianganzo@yahoo.com.br>
2023-10-12 16:40:28 -0300

    Exercicio 3 - Resolvido

--- index.ts
+++ index.ts
@@ -35,6 +35,36 @@ const listarUsuarios = (): Usuario[] => {
   return lerArquivo() as Usuario[];
 }
 
+const detalharUsuario = (cpf: string): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+  const usuario = bd.find(usuario => {
+    return usuario.cpf === cpf;
+  });
+
+  if (!usuario) {
+    throw new Error("Usuário não encontrado");
+  }
+
+  return usuario;
+}
+
+const atualizarUsuario = (cpf: string, dados: Usuario): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+  const usuario = bd.find(usuario => {
+    return usuario.cpf === cpf;
+  });
+
+  if (!usuario) {
+    throw new Error("Usuário não encontrado");
+  }
+
+  Object.assign(usuario, dados);
+
+  escreverArquivo(bd);
+
+  return dados;
+}
+
 // const adrian = cadastrarUsuario({
 //   nome: "Adrian",
 //   email: "adrian@email.com",
@@ -47,18 +77,34 @@ const listarUsuarios = (): Usuario[] => {
 //   }
 // });
 
-const guido = cadastrarUsuario({
+// const guido = cadastrarUsuario({
+//   nome: "Guido",
+//   email: "guido@email.com",
+//   cpf: "12345678901",
+//   profissao: "Professor BackEnd",
+//   endereco: {
+//     cep: "12345-678",
+//     rua: "Rua A",
+//     bairro: "Centro",
+//     cidade: "Salvador"
+//   }
+// });
+
+// const guido = detalharUsuario("1234567890");
+
+atualizarUsuario("12345678901", {
   nome: "Guido",
   email: "guido@email.com",
   cpf: "12345678901",
-  profissao: "Professor BackEnd",
+  profissao: "BackEnd",
   endereco: {
     cep: "12345-678",
     rua: "Rua A",
+    complemento: "Casa 05",
     bairro: "Centro",
     cidade: "Salvador"
   }
 });
 
 const bd = lerArquivo();
-console.log(guido, bd);
+console.log(bd);
