8d7f49295c1f45ef2e48e7a54aa642d19def8443
Adrian Ganzo <adrianganzo@yahoo.com.br>
2023-10-12 16:16:30 -0300

    Exercicio 2 - Resolvido

--- index.ts
+++ index.ts
@@ -7,3 +7,58 @@ const lerArquivo = (): unknown => {
 const escreverArquivo = (dados: any): void => {
   fs.writeFileSync("./bd.json", JSON.stringify(dados));
 }
+
+type Endereco = {
+  cep: string,
+  rua: string,
+  complemento?: string,
+  bairro: string,
+  cidade: string
+}
+
+type Usuario = {
+  nome: string,
+  email: string,
+  cpf: string,
+  profissao?: string,
+  endereco: Endereco | null
+}
+
+const cadastrarUsuario = (dados: Usuario): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+  bd.push(dados);
+  escreverArquivo(bd);
+  return dados;
+}
+
+const listarUsuarios = (): Usuario[] => {
+  return lerArquivo() as Usuario[];
+}
+
+// const adrian = cadastrarUsuario({
+//   nome: "Adrian",
+//   email: "adrian@email.com",
+//   cpf: "12345678900",
+//   endereco: {
+//     cep: "88080250",
+//     rua: "Abel Capela",
+//     bairro: "Coqueiros",
+//     cidade: "Florianópolis"
+//   }
+// });
+
+const guido = cadastrarUsuario({
+  nome: "Guido",
+  email: "guido@email.com",
+  cpf: "12345678901",
+  profissao: "Professor BackEnd",
+  endereco: {
+    cep: "12345-678",
+    rua: "Rua A",
+    bairro: "Centro",
+    cidade: "Salvador"
+  }
+});
+
+const bd = lerArquivo();
+console.log(guido, bd);
